6c92a698718ce65919392660b4cee01bf5641af2
Artur Rossi Junior <artur.15.rossi@gmail.com>
2024-04-20 11:25:28 -0300

    

--- src/main/java/DAO/ConexaoDAO.java
+++ src/main/java/DAO/ConexaoDAO.java
@@ -1,18 +1,21 @@
+package DAO;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexaoDAO {
-    private static final String URL = "jdbc:mysql://localhost:1433/Hotelaria";
-    private static final String USUARIO = "sa";
-    private static final String SENHA = "123456";
+    private static final String URL = "jdbc:sqlserver://localhost:1433;databaseName=master;integratedSecurity=true";
+    //private static final String USUARIO = "sa";
+    //private static final String SENHA = "123456";
 
     public static Connection obterConexao() {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection conexao = DriverManager.getConnection(URL, USUARIO, SENHA);
+            Connection conexao = DriverManager.getConnection(URL);
+            //Connection conexao = DriverManager.getConnection(URL, USUARIO, SENHA);
             return conexao;
-        } catch (ClassNotFoundException | SQLException ex) {
+        } catch (SQLException ex) {
+            System.out.println("Erro ao conectar-se ao banco de dados: " + ex.getMessage());
             return null;
         }
     }
--- src/main/java/com/mycompany/hotelaria/App.java
+++ src/main/java/com/mycompany/hotelaria/App.java
@@ -7,10 +7,10 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import DAO.ConexaoDAO;
 
-/**
- * JavaFX App
- */
 public class App extends Application {
 
     private static Scene scene;
@@ -20,6 +20,8 @@ public class App extends Application {
         scene = new Scene(loadFXML("primary"), 640, 480);
         stage.setScene(scene);
         stage.show();
+        
+        testarConexao();
     }
 
     static void setRoot(String fxml) throws IOException {
@@ -34,5 +36,19 @@ public class App extends Application {
     public static void main(String[] args) {
         launch();
     }
-
+    
+    private void testarConexao() {
+        try {
+            Connection conexao;
+            conexao = ConexaoDAO.obterConexao();
+            if (conexao != null) {
+                System.out.println("Conexão com o banco de dados estabelecida com sucesso!");
+                conexao.close(); // Fechando a conexão após o teste
+            } else {
+                System.out.println("Falha ao conectar-se ao banco de dados.");
+            }
+        } catch (SQLException e) {
+            System.out.println("Erro ao conectar-se ao banco de dados: " + e.getMessage());
+        }
+    }
 }
