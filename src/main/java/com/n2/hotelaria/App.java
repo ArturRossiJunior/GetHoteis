9b6f46028259b00fd98887ebd50eac73f169114b
Artur Rossi Junior <artur.15.rossi@gmail.com>
2024-06-06 16:44:37 -0300

    app

--- src/main/java/com/n2/hotelaria/App.java
+++ src/main/java/com/n2/hotelaria/App.java
@@ -14,8 +14,8 @@ public class App extends Application {
     
     @Override
     public void start(Stage stage) throws IOException {
-        //scene = new Scene(loadFXML("Login"));
-        scene = new Scene(loadFXML("Home"));
+        scene = new Scene(loadFXML("Login"));
+        //scene = new Scene(loadFXML("Home"));
         stage.setScene(scene);
         stage.setTitle("Login");
         stage.setMinHeight(400);
